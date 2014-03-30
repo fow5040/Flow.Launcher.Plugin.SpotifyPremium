53fe848948bcf851df0a0ecb1760dd8182f86981
Ioannis G. <jtgftw@gmail.com>
2014-03-31 00:32:37 +0300

    add Spotify icon

--- SpotifyPlugin.cs
+++ SpotifyPlugin.cs
@@ -25,28 +25,31 @@ namespace Wox.Plugin.Spotify
             {
                 case "artist":
                     param = param.Substring("artist ".Length);
-                    return data.GetArtists(param).Artists.Select(x => new Result()
+                    return data.GetArtists(param).Artists.ToList().GetRange(0, 10).Select(x => new Result()
                         {
                             Title = x.Name,
                             SubTitle = string.Format("Popularity: {0}%", System.Convert.ToDouble(x.Popularity)*100 ),
-                            Action = e => _context.ShellRun(x.Href)
+                            Action = e => _context.ShellRun(x.Href),
+                            IcoPath = "icon.png"
                         }).ToList();
                 case "album":
                     param = param.Substring("album ".Length);
-                    return data.GetAlbums(param).Albums.Select(x => new Result()
+                    return data.GetAlbums(param).Albums.ToList().GetRange(0, 10).Select(x => new Result()
                         {
                             Title = x.Name,
                             SubTitle = "Artist: " + string.Join(", ", x.Artists.Select(a => a.Name).ToArray()),
-                            Action = e => _context.ShellRun(x.Href)
+                            Action = e => _context.ShellRun(x.Href),
+                            IcoPath = "icon.png"
                         }).ToList();
                 default:
                     if (query.ActionParameters[0] == "track")
                         param = param.Substring("track ".Length);
-                    return data.GetTracks(param).Tracks.Select(x => new Result()
+                    return data.GetTracks(param).Tracks.ToList().GetRange(0, 20).Select(x => new Result()
                         {
                             Title = x.Name,
                             SubTitle = "Album: " + x.Album.Name,
-                            Action = e => _context.ShellRun(x.Href)
+                            Action = e => _context.ShellRun(x.Href),
+                            IcoPath = "icon.png"
                         }).ToList();
             }
         }
