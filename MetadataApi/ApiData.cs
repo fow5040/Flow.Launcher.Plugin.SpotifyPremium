335c5e977502ae3837e00ce769b090568c8930e5
Ioannis G. <jtgftw@gmail.com>
2014-04-07 18:41:13 +0300

    add comments

--- MetadataApi/ApiData.cs
+++ MetadataApi/ApiData.cs
@@ -8,10 +8,13 @@ namespace Wox.Plugin.Spotify.MetadataApi
 {
     public class ApiData
     {
+        // base URL for track/album/artist data requests
         private const string SPOTIFY_API_URI = "http://ws.spotify.com/search/1/{0}.json?q={1}";        
 
+        // base url to retrieve artwork of any track/album/artist data
         private const string SPOTIFY_EMBED_URI = "https://embed.spotify.com/oembed/?url={0}";
 
+        // path to the plugin directory
         private string PluginDirecotry;
 
         public ApiData(string pluginDir = null)
@@ -19,24 +22,39 @@ namespace Wox.Plugin.Spotify.MetadataApi
             this.PluginDirecotry = pluginDir ?? Directory.GetCurrentDirectory();
         }
 
+        /// <summary>
+        /// Retrieves track data and returns the (deserialized) response
+        /// </summary>
         public TracksApiResponse GetTracks(string searchTerms)
         {
             var data = GetData(searchTerms, SearchType.Track);
             return JsonConvert.DeserializeObject<TracksApiResponse>(data);
         }
 
+        /// <summary>
+        /// Retrieves album data and returns the (deserialized) response
+        /// </summary>
         public AlbumsApiResponse GetAlbums(string searchTerms)
         {
             var data = GetData(searchTerms, SearchType.Album);
             return JsonConvert.DeserializeObject<AlbumsApiResponse>(data);
         }
 
+        /// <summary>
+        /// Retrieves artist data and returns the (deserialized) response
+        /// </summary>
         public ArtistsApiResponse GetArtists(string searchTerms)
         {
             var data = GetData(searchTerms, SearchType.Artist);
             return JsonConvert.DeserializeObject<ArtistsApiResponse>(data);
         }
 
+        /// <summary>
+        /// Creates the request uri based on the specified search terms and search type
+        /// </summary>
+        /// <param name="searchTerms">what to search for</param>
+        /// <param name="type">type of search (track, album or artist)</param>
+        /// <returns>the API response as string</returns>
         public string GetData(string searchTerms, SearchType type)
         {
             var uri = string.Format(SPOTIFY_API_URI, type.ToString().ToLower(), searchTerms);
@@ -44,6 +62,9 @@ namespace Wox.Plugin.Spotify.MetadataApi
             return GetData(uri);
         }
 
+        /// <summary>
+        /// Creates a request to the specified uri and returns the response as string
+        /// </summary>
         public string GetData(string uri)
         {
             var request = WebRequest.Create(uri);
@@ -57,11 +78,17 @@ namespace Wox.Plugin.Spotify.MetadataApi
             return new StreamReader(response.GetResponseStream(), Encoding.UTF8).ReadToEnd();
         }
 
+        /// <summary>
+        /// Retrieve and deserialize artwork information, download the image
+        /// in the Cache folder and return a path to the local file.
+        /// </summary>
+        /// <param name="href">The Spotify URL of any track, album or artist</param>
         public string GetArtwork(string href)
         {
             var uri = string.Format(SPOTIFY_EMBED_URI, href);
             
             var request = (HttpWebRequest)WebRequest.Create(uri);
+            // Set a UserAgent, otherwise Spotify's response will be empty!
             request.UserAgent = "Mozilla/5.0";
             var response = (HttpWebResponse)request.GetResponse();
             if (response.StatusCode != HttpStatusCode.OK || response.GetResponseStream() == null)
@@ -69,15 +96,20 @@ namespace Wox.Plugin.Spotify.MetadataApi
                 return null;
             }
 
+            // Read response stream
             var responseData = new StreamReader(response.GetResponseStream(), Encoding.UTF8).ReadToEnd();
 
+            // get a URL to the thumbnail, which could be an album cover or an artist avatar
             var thumbnailUrl = JsonConvert.DeserializeObject<EmbedApiResponse>(responseData).ThumbnailUrl;
 
+            // use the unique spotify ID as the local file name
             var uniqueId = href.Substring(href.LastIndexOf(":") + 1);
 
+            // local path to the image file, located in the Cache folder
             var path = string.Format(@"{0}\{1}.jpg", CacheFodler, uniqueId);
 
             //TODO: Optimize...
+            // Download the image file            
             if (!File.Exists(path))
                 new WebClient().DownloadFile(new Uri(thumbnailUrl), path);
 
--- SpotifyPlugin.cs
+++ SpotifyPlugin.cs
@@ -14,6 +14,7 @@ namespace Wox.Plugin.Spotify
         {
             this._context = context;
 
+            // initialize data, passing it the plugin directory
             data = new ApiData(_context.CurrentPluginMetadata.PluginDirecotry);
         }
 
@@ -21,33 +22,40 @@ namespace Wox.Plugin.Spotify
         {
             var param = query.GetAllRemainingParameter();
 
+            // check if this is an album or artist search, default to track search
             switch (query.ActionParameters[0])
             {
                 case "artist":
                     param = param.Substring("artist ".Length);
+                    // Retrieve data and return the first 10 results
                     return data.GetArtists(param).Artists.ToList().GetRange(0, 10).Select(x => new Result()
                         {
                             Title = x.Name,
                             SubTitle = string.Format("Popularity: {0}%", System.Convert.ToDouble(x.Popularity)*100 ),
+                            // When selected, open it with the spotify client
                             Action = e => _context.ShellRun(x.Href),
                             IcoPath = "icon.png"
                         }).ToList();
                 case "album":
                     param = param.Substring("album ".Length);
+                    // Retrieve data and return the first 10 results
                     return data.GetAlbums(param).Albums.ToList().GetRange(0, 10).Select(x => new Result()
                         {
                             Title = x.Name,
                             SubTitle = "Artist: " + string.Join(", ", x.Artists.Select(a => a.Name).ToArray()),
+                            // When selected, open it with the spotify client
                             Action = e => _context.ShellRun(x.Href),
                             IcoPath = data.GetArtwork(x.Href)
                         }).ToList();
                 default:
                     if (query.ActionParameters[0] == "track")
                         param = param.Substring("track ".Length);
+                    // Retrieve data and return the first 20 results
                     return data.GetTracks(param).Tracks.ToList().GetRange(0, 20).Select(x => new Result()
                         {
                             Title = x.Name,
                             SubTitle = "Album: " + x.Album.Name,
+                            // When selected, open it with the spotify client
                             Action = e => _context.ShellRun(x.Href),
                             IcoPath = "icon.png"
                         }).ToList();
