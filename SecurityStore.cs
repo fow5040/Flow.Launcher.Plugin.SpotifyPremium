372a2c5b6b55df5bd60c7d234607023d5d881380
Frank Walsh <fow5040@yahoo.com>
2019-07-21 00:36:23 -0400

    "Added personal Client Id/Secret"

--- SecurityStore.cs
+++ SecurityStore.cs
@@ -12,15 +12,21 @@ namespace Wox.Plugin.Spotify
     {
         public String RefreshToken { get; set; }
         public Boolean HasRefreshToken { get { return !String.IsNullOrEmpty(RefreshToken); } }
-        public String ClientSecret { get; set; }
+        public String ClientSecret { get; set;}
         public String ClientId {get; set;}
+
         public static SecurityStore Load()
         {
             if(new FileInfo("security.store").Exists)
             {
                 return JsonConvert.DeserializeObject<SecurityStore>(File.ReadAllText("security.store"));
             }
-            return new SecurityStore();
+            //Including personal ClientID and Secret
+            //May remove if potential App/API usage gets out of hand
+            SecurityStore _securityStore = new SecurityStore();
+            _securityStore.ClientId = "41544e27545e4196bdfb6ff3c90d4451";
+            _securityStore.ClientSecret = "c575cd3b2a2347eb846b28f35d541ace";
+            return _securityStore;
         }
 
         public void Save()
